9453f3febbcd2049b40eb76b26c0ce94b6d91790
kudoh <noboru.kudoh@gmail.com>
2019-05-25 17:05:08 +0900

    write local ip to confirm processing pod

--- ingress/app1/routes/index.js
+++ ingress/app1/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var ip = require('ip');
 
 router.get('/', (req, res) => {
-  res.status(200).send('[app1]私の戦闘力は530000です…ですが、もちろんフルパワーであなたと戦う気はありませんからご心配なく…');
+  res.status(200).send(`[app1:${ip.address()}]私の戦闘力は530000です…ですが、もちろんフルパワーであなたと戦う気はありませんからご心配なく…`);
 });
 
 module.exports = router;
--- ingress/app2/routes/index.js
+++ ingress/app2/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var ip = require('ip');
 
 router.get('/', (req, res) => {
-  res.status(200).send('[app2]ずいぶんムダな努力をするんですね・・・そんなことがわたしに通用するわけがないでしょう！');
+  res.status(200).send(`[app2:${ip.address()}]ずいぶんムダな努力をするんですね・・・そんなことがわたしに通用するわけがないでしょう！`);
 });
 
 module.exports = router;
