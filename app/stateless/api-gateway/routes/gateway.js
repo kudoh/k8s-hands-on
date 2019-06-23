3aa193ef1b29df3849433c599d1b1b3367198a6c
kudoh <noboru.kudoh@gmail.com>
2019-06-23 10:26:11 +0900

    use redis cache

--- app/stateless/api-gateway/routes/gateway.js
+++ app/stateless/api-gateway/routes/gateway.js
@@ -1,18 +1,47 @@
 const express = require('express');
-const axios = require('axios')
+const Redis = require('ioredis');
+const axios = require('axios');
+const JSONCache = require('redis-json');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+
 const serviceURL = process.env.SERVICE_URL || 'http://localhost:8080/github/'
+const redisPort = process.env.REDIS_PORT || 6379
+const redisHost = process.env.REDIS_HOST || 'localhost'
+const redisPassword = process.env.REDIS_PASSWORD || null
+
+const redis = new Redis({
+  port: redisPort,
+  host: redisHost,
+  password: redisPassword
+});
+ 
+const jsonCache = new JSONCache(redis, {prefix: 'cache:'});
 
 const client = axios.create({
   baseURL: serviceURL,
   timeout: 5000
 });
 
-router.get('/repos', check('query').not().isEmpty(), checkValidationResult, function(req, res, next) {
-  console.log(`serach parameter: ${req.query.query}`);
-  client.get(`/repos?query=${req.query.query}`)
-    .then(result => {
+router.get('/repos', check('query').not().isEmpty(), checkValidationResult, async (req, res, next) => {
+
+  console.log(`received request. serach parameter: ${req.query.query}`);
+
+  const query = req.query.query;
+
+  const response = await jsonCache.get(query);
+
+  if (response) {
+    console.log(`retrieved from Cache. query: ${query}`)  
+    res.status(200).json(response);
+    return;
+  }
+
+  console.log("cache not found. consult to github-service...")
+  client.get(`/repos?query=${query}`)
+    .then(async result => {
+      console.log(`retrieved github data successfully. caching for next request.`)
+      await jsonCache.set(query, result.data, {expire: 60 * 60});
       res.status(200).json(result.data);
     })
     .catch(e => {
@@ -28,5 +57,11 @@ function checkValidationResult(req, res, next) {
   }
   next();
 }
+
+// for Maintenance
+router.post('/clearCache', (req, res) => {
+  jsonCache.clearAll();
+  res.status(200).end();
+})
 
 module.exports = router;
