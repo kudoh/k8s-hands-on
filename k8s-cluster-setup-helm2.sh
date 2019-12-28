c4ab3656ad9590ed5150893c47092832aeca9013
kudoh <noboru.kudoh@gmail.com>
2019-12-28 16:57:54 +0900

    update for k8s 1.17 and centos/8 image

--- k8s-cluster-setup-helm2.sh
+++ k8s-cluster-setup-helm2.sh
@@ -3,6 +3,11 @@
 MASTER_IP=172.16.10.11
 SHARED_DIR=${1:-./local-cluster/shared}
  
+ # kubeconfigのセットアップ。APIサーバのdefaultは6443ポートが開いている。マルチMasterの場合はLBを指定
+# kubectl config set-cluster local-k8s --server=https://${MASTER_IP}:6443 --certificate-authority=${SHARED_DIR}/k8s-ca.crt
+# kubectl config set-credentials tester --token=$(cat ${SHARED_DIR}/token)
+# kubectl config set-context local-k8s-tester --cluster=local-k8s --user=tester --namespace default
+
 kubectl config set-cluster local-k8s --server=https://${MASTER_IP}:6443 \
   --certificate-authority=${SHARED_DIR}/ca.crt --embed-certs
 kubectl config set-credentials admin \
@@ -19,12 +24,22 @@ rc=$?
 if [[ $rc != 0 ]] ;then
   curl -L https://git.io/get_helm.sh | bash
 fi
-# add stable repo
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+
+kubectl apply -f https://raw.githubusercontent.com/kudoh/k8s-hands-on/master/helm/tiller-rbac.yaml
+#helm init --upgrade --service-account tiller
+# for k8s greater than v1.16
+helm init --service-account tiller --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | sed 's@  replicas: 1@  replicas: 1\n  selector: {"matchLabels": {"app": "helm", "name": "tiller"}}@' | kubectl apply -f -
+rc=$?
+if [[ $rc != 0 ]] ;then
+  echo "unable to install helm..."
+  exit 1
+fi
+
+sleep 60
 
 # MetalLB
 kubectl create namespace metallb-system
-helm upgrade metallb --install stable/metallb --namespace metallb-system
+helm install --name metallb stable/metallb --namespace metallb-system
 kubectl apply -f https://raw.githubusercontent.com/kudoh/k8s-hands-on/master/ingress/metallb-configmap.yaml
 rc=$?
 if [[ $rc != 0 ]] ;then
@@ -59,7 +74,7 @@ if [[ $rc != 0 ]] ;then
   echo "unable to install OpenEBS..."
   exit 1
 fi
-# no more needed, openebs generate default resources
+
 # kubectl apply -f https://raw.githubusercontent.com/kudoh/k8s-hands-on/master/storage/cstor-pool-config.yaml
 # kubectl apply -f https://raw.githubusercontent.com/kudoh/k8s-hands-on/master/storage/storageclass.yaml
 
